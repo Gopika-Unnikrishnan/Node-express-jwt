3ae68030455b5757e2a7b732d8bcff4e7becdfa6
Gopika-Unnikrishnan <gopikavenunivas@gmail.com>
2025-05-17 16:17:10 +0530

    Added cors

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
@@ -7,6 +8,11 @@ const PORT=5000
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: "*", // your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // allow specific methods if needed
+    credentials: true               // if you're sending cookies/auth
+  }));
 connectDB();
 
 
